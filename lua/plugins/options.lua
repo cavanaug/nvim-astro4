437f7ad411b4e5861b2ba9e89f93283ef356d284
John Cavanaugh <john.cavanaugh@hp.com>
2024-04-05 23:54:42 -0700

    back to original names

--- lua/plugins/options.lua
+++ lua/plugins/options.lua
@@ -49,7 +49,6 @@ return {
           cmp_enabled = true, -- enable completion at start
           diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
           icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-          resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
           ui_notifications_enabled = true, -- disable notifications when toggling UI elements
           vim_json_conceal = 0,
         },
