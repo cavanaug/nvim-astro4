a55d5e2b8e560286ddc1205e643c22fb8957cc57
John Cavanaugh <john.cavanaugh@hp.com>
2024-04-07 22:39:59 -0700

    Ufo config

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,37 +1,8 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "kevinhwang91/nvim-ufo",
-  event = { "User AstroFile", "InsertEnter" },
-  dependencies = { "kevinhwang91/promise-async" },
-  opts = {
-    preview = {
-      mappings = {
-        scrollB = "<C-b>",
-        scrollF = "<C-f>",
-        scrollU = "<C-u>",
-        scrollD = "<C-d>",
-      },
-    },
-    provider_selector = function(_, filetype, buftype)
-      local function handleFallbackException(bufnr, err, providerName)
-        if type(err) == "string" and err:match "UfoFallbackException" then
-          return require("ufo").getFolds(bufnr, providerName)
-        else
-          return require("promise").reject(err)
-        end
-      end
-
-      return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
-        or function(bufnr)
-          return require("ufo")
-            .getFolds(bufnr, "lsp")
-            :catch(function(err) return handleFallbackException(bufnr, err, "treesitter") end)
-            :catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
-        end
-    end,
-  },
-  config = function()
-    local handler = function(virtText, lnum, endLnum, width, truncate)
+  opts = function(plugin, opts)
+    opts.fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
       local newVirtText = {}
       local suffix = (" 󰁂 %d "):format(endLnum - lnum)
       local sufWidth = vim.fn.strdisplaywidth(suffix)
@@ -58,16 +29,5 @@ return {
       table.insert(newVirtText, { suffix, "MoreMsg" })
       return newVirtText
     end
-    require("ufo").setup {
-      fold_virt_text_handler = handler,
-    }
   end,
-  -- global handler
-  -- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
-  -- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
-
-  -- buffer scope handler
-  -- will override global handler if it is existed
-  -- local bufnr = vim.api.nvim_get_current_buf()
-  -- require('ufo').setFoldVirtTextHandler(bufnr, handler)
 }
