bacd5254693fd8ed659216bf5f5ec6cb55d0bb56
John Cavanaugh <59479+cavanaug@users.noreply.github.com>
2024-09-05 22:55:39 -0700

    More astro v4 isms

--- lua/community.lua
+++ lua/community.lua
@@ -57,7 +57,7 @@ return {
 
   { import = "astrocommunity.indent.indent-blankline-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
-  -- { import = "astrocommunity.project.project-nvim" },
+  { import = "astrocommunity.project.project-nvim" },
   -- { import = "astrocommunity.utility.noice-nvim" },
   -- { import = "astrocommunity.editing-support.ultimate-autopair-nvim" },
   -- { import = "astrocommunity.editing-support.todo-comments-nvim" },
---
+++ lua/plugins/lsp_custom-astro.lua
@@ -0,0 +1,10 @@
+return {
+  "AstroNvim/astrolsp",
+  ---@type AstroLSPOpts
+  opts = {
+    features = {
+      signature_help = true, -- enable automatic signature help popup globally on startup
+      inlay_hints = true, -- enable inlay hints globally on startup
+    },
+  },
+}
