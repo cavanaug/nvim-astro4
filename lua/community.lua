c0e8e2c79bad64c984112eef34ba93cdae9159e0
John Cavanaugh <59479+cavanaug@users.noreply.github.com>
2024-10-04 14:10:59 -0700

    Layout changes and cleanups in community

--- lua/community.lua
+++ lua/community.lua
@@ -1,11 +1,21 @@
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
+-----------------------------------------------------------------------------------------------------------------------------
+---- Notes:
+---- AstroCommunity: import any community modules here
+---- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+---- This guarantees that the specs are processed before any user plugins.
+-----------------------------------------------------------------------------------------------------------------------------
 
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+
+  -----------------------------------------------------------------------------------------------------------------------------
+  ----  WIP: Items under evaluation
+  -----------------------------------------------------------------------------------------------------------------------------
+  { import = "astrocommunity.motion.mini-surround" },
+  -- { import = "astrocommunity.motion.mini-move" }, --           -- Setup with hydra as a submode??
+
   --
   -- ColorSchemes
   --
@@ -28,75 +38,71 @@ return {
   { "folke/tokyonight.nvim", lazy = false },
   { "Mofiqul/vscode.nvim", lazy = false },
 
-  --
-  --  UI Visual Plugins
-  --
+  -----------------------------------------------------------------------------------------------------------------------------
+  ----  visual - UI Visual Decorations
+  -----------------------------------------------------------------------------------------------------------------------------
   { import = "astrocommunity.scrolling.mini-animate" }, --     -- Motion animation when jumping long distances
   { import = "astrocommunity.bars-and-lines.vim-illuminate" }, -- Highlight other instances of word under cursor
-
-  --
-  --  Mini collection
-  --
+  { import = "astrocommunity.indent.mini-indentscope" }, --    -- This is about motion actions and highlighting indentation
   { import = "astrocommunity.icon.mini-icons" }, --            -- Icons in the gutter
-  { import = "astrocommunity.motion.mini-surround" },
-  -- { import = "astrocommunity.motion.mini-move" }, --           -- Setup with hydra as a submode??
-  -- { import = "astrocommunity.color.mini-hipatterns" },
-  { import = "astrocommunity.indent.mini-indentscope" }, --    -- Indenting is messed up, I need to figure out how to fix it
 
-  --
-  --  LSP / Completion
-  --
+  -- { import = "astrocommunity.color.mini-hipatterns" },
+  -- { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  -- { import = "astrocommunity.indent.indent-blankline-nvim" },   -- Indenting is messed up, I need to figure out how to fix it
+
+  -----------------------------------------------------------------------------------------------------------------------------
+  ----  lsp - LSP / Completion
+  -----------------------------------------------------------------------------------------------------------------------------
   { import = "astrocommunity.completion.cmp-cmdline" },
   { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
 
-  --
-  -- Motion & Editing
-  --
+  -----------------------------------------------------------------------------------------------------------------------------
+  ----  motion - "BUFFER" Motion / Editing / Searching
+  -----------------------------------------------------------------------------------------------------------------------------
   { import = "astrocommunity.motion.flash-nvim" },
+  -- { import = "astrocommunity.motion.leap-nvim" },                         -- Flash is better
   { import = "astrocommunity.motion.marks-nvim" },
   { import = "astrocommunity.editing-support.telescope-undo-nvim" },
   -- { import = "astrocommunity.motion.tabout-nvim" },
-  -- { import = "astrocommunity.motion.leap-nvim" },
   -- { import = "astrocommunity.editing-support.ultimate-autopair-nvim" },
-  -- { import = "astrocommunity.editing-support.todo-comments-nvim" },
 
-  --
-  --  File Searching / Exporation / Navigation
-  --
+  -----------------------------------------------------------------------------------------------------------------------------
+  ----  navigation - "FILE" Navigation / Searching / Exporation / Navigation
+  -----------------------------------------------------------------------------------------------------------------------------
   { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
   { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.motion.harpoon" },
 
-  --
-  -- Other Various Features
-  --
+  -----------------------------------------------------------------------------------------------------------------------------
+  ----  misc - Other Various Features
+  ---        - Possibly split out ai coding and project into different areas
+  -----------------------------------------------------------------------------------------------------------------------------
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.split-and-window.windows-nvim" }, -- TMUX support??
 
   -- { import = "astrocommunity.editing-support.chatgpt-nvim" },
-  -- { import = "astrocommunity.indent.indent-blankline-nvim" },   -- Indenting is messed up, I need to figure out how to fix it
   -- { import = "astrocommunity.project.project-nvim" },
   -- { import = "astrocommunity.debugging.nvim-bqf" },
   -- { import = "astrocommunity.register.nvim-neoclip-lua" },
   -- { import = "astrocommunity.utility.noice-nvim" },
   -- { import = "astrocommunity.git.diffview-nvim" },
 
-  --
-  -- Recipes
-  --
+  -----------------------------------------------------------------------------------------------------------------------------
+  ----  recipe - Predefined setup recipes
+  -----------------------------------------------------------------------------------------------------------------------------
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
 
-  --
-  -- Language Support
-  --
+  -----------------------------------------------------------------------------------------------------------------------------
+  ----  language - Various Language support
+  -----------------------------------------------------------------------------------------------------------------------------
   { import = "astrocommunity.neovim-lua-development.lazydev-nvim" },
 
-  --
-  -- Language Packs
-  --
+  -----------------------------------------------------------------------------------------------------------------------------
+  ----  packs - Language support packs
+  -----------------------------------------------------------------------------------------------------------------------------
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
